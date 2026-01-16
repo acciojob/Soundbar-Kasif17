be04a82b26e0bbcf59be1992b53653f4a065c73e
Mohd Kasif Khan <developer786kasif@gmail.com>
2026-01-16 13:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+onst buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+let currentAudio = null;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const soundFile = button.getAttribute('data-sound');
+        if (currentAudio) {
+            currentAudio.pause();
+        }
+        currentAudio = new Audio(`sounds/${soundFile}`);
+        currentAudio.play();
+    });
+});
+
+stopButton.addEventListener('click', () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0; // Reset to start
+    }
+});
